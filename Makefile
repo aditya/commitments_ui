d626aceed9c8c67677dfc776d5d126c653bdd498
Will Ballard <wballard@mailframe.net>
2013-05-04 11:02:15 -0500

    all in one, much simpler install

--- Makefile
+++
@@ -1,8 +0,0 @@
-
-serve: refresh
-	python -m SimpleHTTPServer 4444
-
-refresh:
-	git submodule init
-	git submodule update
-
---
+++ scripts/install.sh
@@ -0,0 +1,38 @@
+#! /usr/bin/env bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+. "${DIR}/../environment/environment"
+cd "${DIR}/.."
+ROOT="${PWD}"
+
+echo Setting up data directories
+
+if [ ! -d "${COMMITMENTS_ROOT}" ]; then
+  mkdir -p "${COMMITMENTS_ROOT}"
+fi
+if [ ! -d "${NOTIFY_ROOT}" ]; then
+  mkdir -p "${NOTIFY_ROOT}"
+fi
+if [ ! -d "${TOKEN_ROOT}" ]; then
+  mkdir -p "${TOKEN_ROOT}"
+fi
+token init
+notify init
+commitments init
+
+echo Setting up superforker runtime executables
+
+if [ ! -d "${ROOT}/server_runtime" ]; then
+  mkdir -p "${ROOT}/server_runtime"
+fi
+ls "${ROOT}/node_modules/.bin" \
+  | xargs -I % ln -s "${ROOT}/node_modules/.bin/%" "${ROOT}/server_runtime/%"
+ls "${ROOT}/server" \
+  | xargs -I % ln -s "${ROOT}/server//%" "${ROOT}/server_runtime/%"
+
+echo Links complete
+
+superwatcher init
+superwatcher watch "${ROOT}"
+superwatcher environment "${ROOT}/environment/environment"
+superwatcher main superforker 8080 "${ROOT}/server_runtime" "${ROOT}/client"
+superwatcher info
