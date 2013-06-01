43bfe2e4d615245c139b644d2b824c9cf9d9add7
Will Ballard <wballard@mailframe.net>
2013-06-01 08:54:14 -0500

    small optimization to generated app, exlcuding less and coffee

--- client/optimized.js
+++ client/optimized.js
@@ -1,78 +1,78 @@
 //This is the server based requirejs optimization configuration
 module.exports = {
-  logLevel: 0,
-  baseUrl: './',
-  optimize: 'none',
-  cssoptimize: 'node',
-  findNestedDependencies: false,
-  name: 'root',
-  //this needs to be in explicitly, not sure why it isn't picking up
-  deps: ['css'],
-  paths: {
-    md5: 'lib/md5',
-    store: 'lib/store',
-    jquery: 'lib/jquery',
-    jqueryui: 'lib/jqueryui',
-    lodash: 'lib/lodash',
-    socketio: 'lib/socket.io',
-    marked: 'lib/marked',
-    moment: 'lib/moment',
-    grid: 'lib/grid-a-licious',
-    angular: 'lib/angular/angular',
-    'coffee-script': 'lib/coffee-script',
-    cs: 'lib/require/cs',
-    less: 'lib/require/less',
-    text: 'lib/require/text',
-    css: 'lib/require/css',
-    'less-builder': 'lib/require/less-builder',
-    'css-builder': 'lib/require/css-builder',
-    'normalize': 'lib/require/normalize',
-    'lessc-server': 'lib/require/lessc-server',
-    'lessc': 'lib/require/lessc',
-    bootstrap: 'lib/bootstrap/js/bootstrap',
-    tagbar: 'src/widgets/tagbar',
-    lunr: 'lib/lunr',
-    mousetrap: 'lib/mousetrap',
-    codemirror: 'lib/codemirror/lib/codemirror',
-    codemirrormarkdown: 'lib/codemirror/mode/markdown/markdown',
-  },
-  shim: {
-    //export this, allows plug ins to hook on
-    'jquery': {
-      exports: 'jQuery'
+    logLevel: 0,
+    baseUrl: './',
+    optimize: 'none',
+    findNestedDependencies: true,
+    name: 'root',
+    pragmasOnSave: {
+        //Just an example
+        excludeCoffeeScript: true
     },
-    'md5': {
-      exports: 'md5'
+    deps: ['css'],
+    stubModules: ['cs', 'coffee-script', 'lessc' ],
+    paths: {
+        md5: 'lib/md5',
+        store: 'lib/store',
+        jquery: 'lib/jquery',
+        jqueryui: 'lib/jqueryui',
+        lodash: 'lib/lodash',
+        socketio: 'lib/socket.io',
+        marked: 'lib/marked',
+        moment: 'lib/moment',
+        grid: 'lib/grid-a-licious',
+        angular: 'lib/angular/angular',
+        'coffee-script': 'lib/coffee-script',
+        cs: 'lib/require/cs',
+        less: 'lib/require/less',
+        text: 'lib/require/text',
+        css: 'lib/require/css',
+        'less-builder': 'lib/require/less-builder',
+        'css-builder': 'lib/require/css-builder',
+        'normalize': 'lib/require/normalize',
+        'lessc-server': 'lib/require/lessc-server',
+        'lessc': 'lib/require/lessc',
+        bootstrap: 'lib/bootstrap/js/bootstrap',
+        tagbar: 'src/widgets/tagbar',
+        lunr: 'lib/lunr',
+        mousetrap: 'lib/mousetrap',
+        codemirror: 'lib/codemirror/lib/codemirror',
+        codemirrormarkdown: 'lib/codemirror/mode/markdown/markdown',
     },
-    'markdown': {
-      exports: 'markdown'
+    shim: {
+        //export this, allows plug ins to hook on
+        'jquery': {
+            exports: 'jQuery'
+        },
+        'md5': {
+            exports: 'md5'
+        },
+        'markdown': {
+            exports: 'markdown'
+        },
+        //export angular, but make sure jquery is up so we don't get stuck
+        //with jqlite!
+        'angular': {
+            deps: ['jquery'],
+            exports: 'angular'
+        },
+        'bootstrap': {
+            deps: ['jquery']
+        },
+        'jqueryui': {
+            deps: ['jquery']
+        },
+        'tagbar': {
+            deps: ['jquery']
+        },
+        'codemirror': {
+            exports: 'CodeMirror'
+        },
+        'codemirrormarkdown': {
+            deps: ['codemirror']
+        },
+        'lunr': {
+            exports: 'lunr'
+        },
     },
-    //export angular, but make sure jquery is up so we don't get stuck
-    //with jqlite!
-    'angular': {
-      deps: ['jquery'],
-      exports: 'angular'
-    },
-    'bootstrap-notify': {
-      deps: ['jquery']
-    },
-    'bootstrap': {
-      deps: ['jquery']
-    },
-    'jqueryui': {
-      deps: ['jquery']
-    },
-    'tagbar': {
-      deps: ['jquery']
-    },
-    'codemirror': {
-      exports: 'CodeMirror'
-    },
-    'codemirrormarkdown': {
-      deps: ['codemirror']
-    },
-    'lunr': {
-      exports: 'lunr'
-    },
-  },
 }
